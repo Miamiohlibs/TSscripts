40cc6dedb2e8f9abd6c9dda64d64a2c77f2c03ce
Craig Boman <craig.boman@gmail.com>
2017-06-21 11:55:29 -0400

    missing file

--- Test/test-chrome.py
+++ Test/test-chrome.py
@@ -2,4 +2,4 @@ from pywinauto.application import Application
 
 app = Application().Connect(title=u'Sierra \xb7 Miami University Libraries \xb7 Craig Boman - Google Chrome', class_name='Chrome_WidgetWin_1')
 chromewidgetwin = app.Chrome_WidgetWin_1
-chromewidgetwin.ClickInput()
+chromewidgetwin.ClickInput()   	
