63488fbd087cdfb7fe100c74f51638e0f8cd2e35
Craig Boman <craig.boman@gmail.com>
2017-06-23 10:05:16 -0400

    testing

---
+++ Test/python.py
@@ -0,0 +1,58 @@
+from time import sleep
+
+#
+# greeting = 'hello'
+# name = input("Please enter our name")
+# print(greeting + " " + name)
+
+# splitString = "this string has been \nsplit over\nlines"
+# print(splitString)
+# sleep(3)
+#
+# anotherSplitString = """This string has
+# been split
+# over three lines"""
+# print(anotherSplitString)
+
+
+# a = 12
+# b = 3
+# c = a + b
+# d = c / 3
+# e = d - 4
+# print(e*12)
+#
+# parrot = "norwegian blue"
+# print(parrot)
+# print(parrot[-3])
+# print(parrot[0:6])
+#
+# today = "friday"
+# print("day" in today)
+# print("parrot" in today)
+
+
+# age = 24
+# print("my age is " + str(age) + " years")
+#
+# print("My age is {0} years".format(age))
+# print("there are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, {7}".format(31, "january", "mar", "may","jul","aug","oct","dec"))
+#
+
+# print("""Jan: {2}
+# feb: {0}
+# mar: {2}
+# apr: {1}
+# may: {2}
+# jun: {1}
+# jul: {2}
+# aug: {2}
+# sep: {1}
+# oct: {2}
+# nov: {1}
+# dec: {2}""".format(28,30,31))
+#
+
+# for i in range(1,12):
+#     print("no. %2d squared is %4d and cubed is %4d" %(i, i**2, i**3))
+
