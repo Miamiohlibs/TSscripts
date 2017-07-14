759ef8f9997eb3d793573113d2ba4ffac3e7926c
Craig Boman <craig.boman@gmail.com>
2017-07-14 09:02:31 -0400

    python training

--- Test/python.py
+++ Test/python.py
@@ -1,4 +1,4 @@
-from time import sleep
+# from time import sleep
 
 #
 # greeting = 'hello'
@@ -36,7 +36,8 @@ from time import sleep
 # print("my age is " + str(age) + " years")
 #
 # print("My age is {0} years".format(age))
-# print("there are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, {7}".format(31, "january", "mar", "may","jul","aug","oct","dec"))
+# print("there are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, {7}"
+#       .format(31, "january", "mar", "may","jul","aug","oct","dec"))
 #
 
 # print("""Jan: {2}
@@ -100,15 +101,15 @@ from time import sleep
 #     print("x in box")
 # else:
 #     print("please come back in {0}".format(18-age))
-
-#6.31
+#
+# 6.31
 # for i in range(1,20):
 #     print("i is now {}".format(i))
 #
 # number = "9,223,372,036,854,775,807"
 # for i in range(0, len(number)):
 #     print(number[i])
-
+#
 # #6.5
 # number = "9,223,372,036,854,775,807"
 # cleanedNumber = ''
@@ -120,7 +121,7 @@ from time import sleep
 # newNumber = int(cleanedNumber)
 # print("the number is {}".format(newNumber))
 
-#6.6
+# 6.6
 # number = "9,223,372,036,854,775,807"
 # cleanedNumber = ''
 #
@@ -144,7 +145,7 @@ from time import sleep
 #     print('')
 
 
-#6.7
+# 6.7
 # shopping_list = ["milk","pasta","eggs","spam","bread","rice"]
 # for item in shopping_list:
 #     if item == 'spam': #if loop takes spam out of the output list; skips it
@@ -152,8 +153,8 @@ from time import sleep
 #         #continue
 #         break #breaks out of the loop when condition is met
 #     print("buy " + item)
-
-#below code breaks the for loop prior to printing, to save processing if condition is met
+#
+# below code breaks the for loop prior to printing, to save processing if condition is met
 # meal = ["egg", "bacon","spam","sausages"]
 #
 # nasty_food_item = ''
@@ -166,13 +167,137 @@ from time import sleep
 # if nasty_food_item:
 #     print("can't I have anything without spam in it? ")
 
-#6.8
-number = "9,223,372,036,854,775,807"
-cleanedNumber = ''
+# 6.8 - augmented assignment - use augmented assignment whereever possible
+# number = "9,223,372,036,854,775,807"
+# cleanedNumber = ''
+#
+# for i in range(0, len(number)):
+#     if number[i] in '0123456789':
+#         cleanedNumber += number[i]
+#
+# newNumber = int(cleanedNumber)
+# print("the number is {}".format(newNumber))
+#
+# x = 23
+# x += 1
+# print(x)
+#
+# x -= 4
+# print(x)
+#
+# x *= 5
+# print(x)
+#
+# x /= 4
+# print(x)
+#
+# x **= 2 #to the power of  2
+# print(x)
+#
+# x %= 60
+# print(x)
+#
+# greeting = "Good "
+# greeting += "morning"
+# print(greeting)
+#
+# greeting *= 5
+# print(greeting)
 
-for i in range(0, len(number)):
-    if number[i] in '0123456789':
-        cleanedNumber += number[i]
 
-newNumber = int(cleanedNumber)
-print("the number is {}".format(newNumber))
+# 6.9 section challenge - program flow
+
+# ipAddress = input("please enter an ip address: ")
+# if ipAddress[-1] != '.':
+#     ipAddress += '.'
+# # ip_list = [".123.45.678.91", "123.4567.8.9", "123.156.289.10123456", "10.10t.10.10", "12.9.34.6.12.90", ""]
+#
+# segment = 1
+# segment_length = 0
+# # character = ""
+#
+# for character in ipAddress:
+#     if character == '.':
+#         print("segment {} contains {} characters".format(segment, segment_length))
+#         segment += 1
+#         segment_length = 0
+#     else:
+#         segment_length += 1
+
+
+
+
+#6.11 constrasting for loops and while loops (while take more code, and might loop forever in some cases)
+# for i in range(10):
+#     print("i is now {}".format(i))
+
+# i = 0
+# while i < 10:
+#     print("i is now {}".format(i))
+#     i += 1
+
+#this is a good application for while loops when you don't know how much data is available (good for reading files)
+#
+
+#6.12 random number challenge/game
+#
+# import random
+#
+# highest = 1000 #you mostly guess the random within 10 guesses, based on splitting the range(1,10), you'd guess 5, 3,
+# answer = random.randint(1, highest)
+#
+# print("please guess a number between 1 and {}".format(highest))
+# guess = 0 #initialized value must be outside of valid range in while loop
+# while guess != answer:
+#     guess = int(input())
+#     if guess < answer:
+#         print("please guess higher")
+#     elif guess > answer:
+#         print("please guess lower")
+#     else:
+#         print("well done, you guessed it")
+
+#7.1
+# ipAddress = input("please enter an ip address: ")
+# print(ipAddress.count("."))
+
+#
+
+# parrot_list = ["non pinin'", "no more", "a stiff", "bereft of life"]
+#
+# parrot_list.append("a norwegian blue")
+#
+# for state in parrot_list:
+#     print("this parrot is " + state)
+
+# even = [2, 4, 6, 8]
+# odd = [1, 3, 5, 7]
+#
+# numbers = even + odd
+# #numbers.sort()
+# print(numbers) #can't use print(numbers.sort), since it doesn't return a value, method operating on object.
+# # intentional by design use numbers.sort()
+# print(sorted(numbers))
+
+
+#7.2 lists
+#
+# list_1 = []
+# list_2 = list()
+#
+# print("list 1: {}".format(list_1))
+# print("list 2: {}".format(list_2))
+#
+# if list_1 == list_2:
+#     print("The lists are equal")
+#
+# print(list("the lists are equal")) #creates a character list based on the input text
+
+
+even = [2, 4, 6, 8]
+
+another_even = even
+another_even.sort(reverse=True) #reverse sort order
+print(even)
+
+print()
